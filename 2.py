2887e2998fb2c060d1cbad1f54c17d1be68416f0
Paradis <Lucasezequiel120202@gmail.com>
2022-12-26 13:37:19 -0300

    join explain

--- 2.py
+++ 2.py
@@ -1,3 +1,10 @@
 s = "Martina iturbe Luna biocca Camila dongiovanni Julieta dominici Jeronimo ballestrin"
 
-print(c)
+ss = s.split()
+print(ss)
+sss = [" ".join(ss[i:i+2]) for i in range(0, len(ss), 2)]
+s1 = []
+for i in range(0,len(ss), 2):
+    s1.append(ss[i] + " " + ss[i+1])
+    
+print(s1)
