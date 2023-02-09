3a263f65f8cecf4872036736cc9b6290cb476033
Paradis <Lucasezequiel120202@gmail.com>
2023-02-09 00:48:42 -0300

    fix to header problem

--- 0.1.py
+++ 0.1.py
@@ -13,9 +13,9 @@ from paramiko import SSHClient
 from scp import SCPClient
 from dotenv import load_dotenv
 import os
+import json
 
-
-folder = "a"
+folder = "negocioCholo"
 
 
 load_dotenv()
@@ -35,7 +35,7 @@ time.sleep(10)
 print("starting")
 cont = 1
 
-p = Path("./valoresWsBot.xlsx")
+p = Path(f"./backup/{folder}.xlsx")
 dataExcel = openpyxl.load_workbook(p)
 sheet = dataExcel.active
 
@@ -66,12 +66,15 @@ def clickNextButton():
             By.XPATH, '//*[@id="app"]/div/span[3]/div/div/div[2]/div/div[1]/div[2]/div/div[7]/div/span').click()
 
 
-keys = ['ShYSmkGi19ztn5G7oDBPLUbN ', 'gcLYS11Z6jrL7MQP65mZ2y7C ', 'ds4c2TkgKKNZwJ1qSJ4sCtAG ',
-        '5awd6A82Jthc1jA383R9z3VP ', 'zMPPPMiD2GCPVMDmfDdNDeLh ', 'CHjwZxPhVxRHbKPvWQhFCnFA ', 'eoSRfgSTAN75o6AQ1YUQ87h4 ']
+keys = ['ShYSmkGi19ztn5G7oDBPLUbN', 'gcLYS11Z6jrL7MQP65mZ2y7C', 'ds4c2TkgKKNZwJ1qSJ4sCtAG',
+        '5awd6A82Jthc1jA383R9z3VP', 'zMPPPMiD2GCPVMDmfDdNDeLh', 'eoSRfgSTAN75o6AQ1YUQ87h4']
 currentKey = 0
 
+def has0left():
+    global currentKey
 
-def has0left(key):
+    print("3")
+
     headers = {
         'accept': '*/*',
         'X-API-Key': keys[currentKey],
@@ -81,30 +84,37 @@ def has0left(key):
         'https://api.remove.bg/v1.0/account', headers=headers)
     callsLeft = json.loads(response.text)[
         "data"]["attributes"]["api"]["free_calls"]
+    print("4")
 
     # print(response.text, keys[currentKey])
-    # print(f"found {keys[currentKey]} with {callsLeft} left")
+    print(f"found {keys[currentKey]} with {callsLeft} left")
+
+    print("5")
 
     return callsLeft == 0
 
 
 def cleanIMG():
+    global currentKey
 
-    while has0left(currentKey):
+    print("1")
+    while has0left():
+        print("2")
+
         if currentKey == len(keys) - 1:
             currentKey = 0
         else:
             currentKey += 1
 
     try:
+        print("6")
+
         print("using key: " + keys[currentKey])
         response = requests.post(
             'https://api.remove.bg/v1.0/removebg',
-            files={'image_file': open(f"images/img{cont}.{subcont}.png", 'rb')},
+            files={'image_file': open(f"backup/{folder}/img{cont}.{subcont}.png", 'rb')},
             data={'size': 'auto'},
-            headers={'X-Api-Key': keys[currentKey], 'bg_color': (None, 'white'),
-                     'type': (None, 'product'),
-                     'type_level': (None, '1')}
+            headers={'X-Api-Key': keys[currentKey]}
         )
 
         if response.status_code == requests.codes.ok:
@@ -113,6 +123,16 @@ def cleanIMG():
         else:
             print("Error:", response.status_code, response.text)
             time.sleep(3)
+
+        try:
+            img = Image.open(f"backup/{folder}/img{cont}.{subcont}.png").convert("RGBA")
+            x, y = img.size
+            card = Image.new("RGBA", (x, y), (255, 255, 255))
+            card.paste(img, (0, 0, x, y), img)
+            card.save(f"backup/{folder}/img{cont}.{subcont}.png", format="png")
+        except Exception as e:
+            print("error pasting white bg")
+            print(e)
 
     except Exception as e:
         print("error cleaning img:")
@@ -199,7 +219,7 @@ while True:
             newLine = True
             subcont += 1
 
-        with open(f"images/img{cont}.{subcont}.png", 'wb') as f:
+        with open(f"backup/{folder}/img{cont}.{subcont}.png", 'wb') as f:
             f.write(base64.b64decode(result))
 
         cleanIMG()
@@ -210,7 +230,7 @@ while True:
 
         time.sleep(1)
         print("uploading")
-        scp.put(f"images/img{cont}.{subcont}.png",
+        scp.put(f"backup/{folder}/img{cont}.{subcont}.png",
                 f"/root/wsbot/backup/{folder}/img{cont}.{subcont}.png")
         ip = f"http://50.116.47.159/backup/{folder}/img{cont}.{subcont}.png"
         ips.append(ip)
@@ -230,7 +250,7 @@ while True:
     except Exception as e:
         print("error")
         print(e)
-        # traceback.print_exc()
+        traceback.print_exc()
         time.sleep(1)
     finally:
         scp.close()
